8ec8ea949331fdce3b72499f591ab8202bb8ec33
choibumku00 <101037541+choibumku00@users.noreply.github.com>
2023-07-27 16:34:42 +0900

    영상을 클릭하면 tab4에서 큰화면으로 영상을 볼 수 있도록 함, 하지만 버튼 클릭시 이슈

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import json
 import queue
 import re
 
+# 본 영상을 저장하는 리스트
+watchedVideo=list()
+
 # 유튜브 검색시 유튜브 리스트 저장
 class YoutubeVideo:
     youtube_list = list()
@@ -18,7 +21,7 @@ class YoutubeVideo:
         self.url=url
         self.desc=desc
         self.duration=duration
-        self.watch=None
+        self.watch=False
         self.segment=None
         self.youtube_list.append(self)
     
@@ -57,7 +60,6 @@ class Script_Exctractor:
         ret.put(sentence)
         while not ret.empty():
             self.scriptData.append(ret.get())
-        print("done")
 
         for i in range(len(self.scriptData)):
             text = self.scriptData[i].replace(u'\xa0', u' ').replace(u'\n',u' ').replace(u'  ',u' ')
@@ -93,7 +95,6 @@ class Script_Exctractor:
         result = []
         for annotation in sorted_data:
             if num < numberOfKCs:
-                print("%s (%s) %s" % (annotation["title"], annotation["url"], annotation['pageRank']))
                 result.append({"title":annotation["title"],"url":annotation["url"],"pageRank":annotation["pageRank"]})
 
             num += 1
@@ -114,12 +115,12 @@ class Script_Exctractor:
 
         wiki_data = pd.DataFrame()
         seg_no = 1
-        print(results)
 
         for seg_item in results:
             seg_index = range(0,len(seg_item))
             seg_df = pd.DataFrame(seg_item,index = seg_index)
             seg_df['seg_no'] = seg_no
+            seg_df['understand']=0
             wiki_data = pd.concat([wiki_data,seg_df])
             seg_no = seg_no + 1
         return wiki_data
@@ -172,7 +173,7 @@ def duration_to_minutes(duration_str):
     return total_minutes
 
 def search_youtubes(query):
-    VIDEO_COUNT=10 # 유튜브에서 들고 올 영상 수
+    VIDEO_COUNT=5 # 유튜브에서 들고 올 영상 수
     PREFIX_YOUTUBE_URL = "https://www.youtube.com/watch?v="
     YoutubeVideo.list_reset()
     
@@ -205,8 +206,6 @@ def search_youtubes(query):
             desc = utils.truncate_text ( item['snippet']['description'] )
             video_init = YoutubeVideo(name=name,url=url,desc=desc,duration=duration)
     
-    #test
-    #print(YoutubeVideo.youtube_list[0].url)
     return YoutubeVideo.youtube_list
 
 
@@ -230,7 +229,6 @@ user_input = get_text()
 
 if user_input:
     video_list = search_youtubes(user_input) # 검색한 영상 받아온 리스트    # video_list 변수는 사용하진 않지만 가비지 컬렉터가 안돌도록 저장
-    print(len(YoutubeVideo.youtube_list))
     # 위키화 및 랭크
     for index, video in enumerate(YoutubeVideo.youtube_list):
         try:
@@ -241,11 +239,12 @@ if user_input:
             print(ex)
             print(f"{index}번째 영상 삭제")
             YoutubeVideo.youtube_list.pop(index)
+    print(f"영상 개수: {len(YoutubeVideo.youtube_list)}")
 
 # 페이지 1, 2, 3 
-tab1, tab2, tab3 = st.tabs(["New Learning", "Uncomprehended", "Completed"])
+tab1, tab2, tab3, tab4  = st.tabs(["New Learning", "Uncomprehended", "Completed", "Watch Video"])
 
-PREFIX_YOUTUBE_URL = "https://www.youtube.com/watch?v="
+selected_video = None
 
 #검색된 영상들
 with tab1:
@@ -258,8 +257,12 @@ with tab1:
         cols = st.columns(NUM_OF_VIDOES_PER_EACH_ROW)
         for idx, item in enumerate(YoutubeVideo.youtube_list[r*NUM_OF_VIDOES_PER_EACH_ROW:r*NUM_OF_VIDOES_PER_EACH_ROW+NUM_OF_VIDOES_PER_EACH_ROW]):
             with cols[idx]:
-                st.video(item.url) # 영상 띄우기
-
+                if st.button(f"Watch: {item.name}"):  
+                    selected_video = item  # 클릭한 영상 정보 저장
+                    item.watch=True
+                    watchedVideo.append(item) # 클릭 영상 리스트에 저장
+                
+                st.video(item.url) # 영상 표시
                 extract_concepts(item.url)
 
                 st.write(f"**{item.name}**")
@@ -269,7 +272,7 @@ with tab1:
 with tab2:
     st.header("Uncomprehended Videos")
 
-    for video in YoutubeVideo.youtube_list: #현재는 유튜브 리스트지만 추후 시청한 영상 리스트로 변경
+    for video in watchedVideo: #현재는 유튜브 리스트지만 추후 시청한 영상 리스트로 변경
         if video.segment is not None:
             video_column, info_column = st.columns([2, 3])
             
@@ -284,9 +287,19 @@ with tab2:
 with tab3:
     st.header("Completed Videos")
 
-    for video in YoutubeVideo.youtube_list: #현재는 유튜브 리스트지만 추후 시청한 영상 리스트로 변경
+    for video in watchedVideo: #현재는 유튜브 리스트지만 추후 시청한 영상 리스트로 변경
         if video.segment is not None:
             st.subheader(video.segment)
 
 with open('style.css', 'rt', encoding='UTF8') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True )
+
+#클릭한 영상을 크게 보여주는 탭
+with tab4:
+    st.header("Watch Video")
+
+    if selected_video:
+        st.subheader(selected_video.name)
+        st.video(selected_video.url)
+    else:
+        st.write("Click on a video in 'New Learning Videos' tab to watch it here.")
